ae9252f2400ca525aee5142a0da742946dc94034
SamariumCode <cactuspydon@gmail.com>
2024-05-11 23:46:30 +0330

    Browsable api

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 
+    path('products/', views.product_list),
+    path('products/<str:pk>', views.product_detail),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,2 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view()
+def product_list(request):
+    return Response('Hi')
+
+
+@api_view()
+def product_detail(request, pk):
+    return Response(pk)
