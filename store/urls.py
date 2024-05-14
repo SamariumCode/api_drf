b71ccf988b82eceb003a684d566aeddba6d56b25
SamariumCode <cactuspydon@gmail.com>
2024-05-14 09:44:14 +0330

    Api show product detail

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('products/', views.product_list),
-    path('products/<str:pk>', views.product_detail),
+    path('products/<int:pk>', views.product_detail),
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view()
@@ -9,4 +15,13 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, pk):
-    return Response(pk)
+
+    product = get_object_or_404(Product, pk=pk)
+
+    # try:
+    #     product = Product.objects.get(pk=pk)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)  # Not Found
+
+    serializer = ProductSerializer(product)  # convert to json
+    return Response(serializer.data)
