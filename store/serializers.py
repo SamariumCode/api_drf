70f94d190f074b357d717835ce8e05cc7929005a
SamariumCode <cactuspydon@gmail.com>
2024-05-14 10:26:41 +0330

    Api serializer custom method field

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,10 @@
+from decimal import Decimal
 from rest_framework import serializers
+
+from store.models import Product
+
+
+DOLLORS_TO_RIALS = 500000
 
 
 class ProductSerializer(serializers.Serializer):
@@ -6,4 +12,16 @@ class ProductSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(
         max_digits=6, decimal_places=2)
+
+    unit_price_after_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
     inventory = serializers.IntegerField()
+
+    # Custom Method Field
+    price_rials = serializers.SerializerMethodField()
+
+    def get_price_rials(self, product: Product):
+        return int(product.unit_price * DOLLORS_TO_RIALS)
+
+    def calculate_tax(self, product: Product):
+        return round(product.unit_price * Decimal(1.09), 2)
