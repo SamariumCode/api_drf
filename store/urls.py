04dba11d008814c4cada3c1dd959a83859b0a354
SamariumCode <cactuspydon@gmail.com>
2024-05-16 18:47:36 +0330

    Api ModelViewSet  , ProductViewSet , CategoryViewSet

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('products/', views.ProductList.as_view()),
+    path('products/', views.ProductViewSet.as_view()),
     path('products/<int:pk>', views.ProductDetail.as_view()),
     path('categories/<int:pk>', views.CategoryDetail.as_view(),
          name='category-detail'),
--- store/views.py
+++ store/views.py
@@ -6,37 +6,21 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
 
 
-class ProductList(ListCreateAPIView):
-
+class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
-
-    # def get_serializer_class(self):
-    #     return ProductSerializer
-
     queryset = Product.objects.order_by('id').select_related(
         'category').prefetch_related('discounts').all()
-
-    # def get_queryset(self):
-    #     return Product.objects.filter(name__istartswith='S').select_related('category').prefetch_related('discounts').all()
 
     def get_serializer_context(self):
         return {'request': self.request}
 
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-
-    serializer_class = ProductSerializer
-
-    queryset = Product.objects.select_related('category').all()
-
-    # lookup_field = 'id'
-
-    def delete(self, request, pk):
+    def destroy(self, request, pk):
         product = get_object_or_404(
             Product.objects.select_related('category'), pk=pk)
         if product.order_items.count() > 0:
@@ -48,18 +32,11 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CategoryList(ListCreateAPIView):
+class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     queryset = queryset = Category.objects.filter().prefetch_related('products').all()
 
-
-class CategoryDetail(RetrieveUpdateDestroyAPIView):
-
-    serializer_class = CategorySerializer
-
-    queryset = Category.objects.prefetch_related('products').all()
-
-    def delete(self, request, pk):
+    def destroy(self, request, pk):
         category = get_object_or_404(
             Category.objects.prefetch_related('products'), pk=pk)
         if category.products.count() > 0:
