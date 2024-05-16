d13d4fad3d318a982a6d58f5e822b4c3219e8fb5
SamariumCode <cactuspydon@gmail.com>
2024-05-16 19:01:51 +0330

    Api SimpleRouter in routers

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,29 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
 from . import views
 
-urlpatterns = [
 
-    path('products/', views.ProductViewSet.as_view()),
-    path('products/<int:pk>', views.ProductDetail.as_view()),
-    path('categories/<int:pk>', views.CategoryDetail.as_view(),
-         name='category-detail'),
-    path('categories/', views.CategoryList.as_view(), name='category-list'),
+router = SimpleRouter()
+router.register('products', views.ProductViewSet,
+                basename='product')  # product_list, product_detial
+router.register('categories', views.CategoryViewSet,
+                basename='category')  # category_list, category_detial
 
-]
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('', include(router.urls))
+# ]
+
+
+# urlpatterns = [
+
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>', views.ProductDetail.as_view()),
+#     path('categories/<int:pk>', views.CategoryDetail.as_view(),
+#          name='category-detail'),
+#     path('categories/', views.CategoryList.as_view(), name='category-list'),
+
+# ]
