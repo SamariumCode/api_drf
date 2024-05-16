f8b206eabd6c49b6cf5b7e5a5b1b96b6d761f395
SamariumCode <cactuspydon@gmail.com>
2024-05-16 19:11:04 +0330

    Api DefaultRouter, routers

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
 from . import views
 
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('products', views.ProductViewSet,
                 basename='product')  # product_list, product_detial
 router.register('categories', views.CategoryViewSet,
@@ -15,15 +15,4 @@ urlpatterns = router.urls
 
 # urlpatterns = [
 #     path('', include(router.urls))
-# ]
-
-
-# urlpatterns = [
-
-#     path('products/', views.ProductList.as_view()),
-#     path('products/<int:pk>', views.ProductDetail.as_view()),
-#     path('categories/<int:pk>', views.CategoryDetail.as_view(),
-#          name='category-detail'),
-#     path('categories/', views.CategoryList.as_view(), name='category-list'),
-
 # ]
