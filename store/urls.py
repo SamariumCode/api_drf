6a0a01c449bb9b509332eec2c9e5d6d05369cdd9
SamariumCode <cactuspydon@gmail.com>
2024-05-16 12:02:22 +0330

    Api CBV APIView

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    path('products/', views.product_list),
-    path('products/<int:pk>', views.product_detail),
-    path('categories/<int:pk>', views.category_detail, name='category-detail'),
-    path('categories/', views.category_list, name='category-list'),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>', views.ProductDetail.as_view()),
+    path('categories/<int:pk>', views.CategoryDetail.as_view(),
+         name='category-detail'),
+    path('categories/', views.CategoryList.as_view(), name='category-list'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,47 +1,48 @@
+from django.db.models import Count
 from django.shortcuts import get_object_or_404
-
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-from django.db.models import Count
+from rest_framework.views import APIView
 
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.filter(
             name__istartswith='S').select_related('category').prefetch_related('discounts')
-
         serializer = ProductSerializer(
             queryset, many=True, context={'request': request})
-
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):
-
-    product = get_object_or_404(
-        Product.objects.select_related('category'), pk=pk)
-
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(
+            Product.objects.select_related('category'), pk=pk)
         serializer = ProductSerializer(
             product, context={'request': request})  # convert to json
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk):
+        product = get_object_or_404(
+            Product.objects.select_related('category'), pk=pk)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk):
+        product = get_object_or_404(
+            Product.objects.select_related('category'), pk=pk)
         if product.order_items.count() > 0:
             return Response({'error': 'There is some order items including this product please remove the first'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -51,9 +52,8 @@ def product_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def category_list(request):
-    if request.method == 'GET':
+class CategoryList(APIView):
+    def get(self, request):
         queryset = Category.objects.filter().annotate(
             products_count=Count('products')).all()
 
@@ -61,32 +61,33 @@ def category_list(request):
             queryset, many=True, context={'request': request})
 
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def category_detail(request, pk):
-
-    category = get_object_or_404(Category.objects.filter().annotate(
-        products_count=Count('products')), pk=pk)
-
-    if request.method == 'GET':
+class CategoryDetail(APIView):
+    def get(self, request, pk):
+        category = get_object_or_404(Category.objects.filter().annotate(
+            products_count=Count('products')), pk=pk)
         serializer = CategorySerializer(category)  # convert to json
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk):
+        category = get_object_or_404(Category.objects.filter().annotate(
+            products_count=Count('products')), pk=pk)
         serializer = CategorySerializer(category, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
-        if Category.objects.filter().select_related('products').count() > 0:
-            return Response({'error': 'There is some products relating  this category. please remove the first'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-        category = get_object_or_404(
-            Category.objects.prefetch_related('products'), pk=pk)
+    def delete(self, request, pk):
+        category = get_object_or_404(Category.objects.filter().annotate(
+            products_count=Count('products')), pk=pk)
+        if category.products_count > 0:
+            return Response({'error': 'There is some products relating  this category. please remove the first'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
